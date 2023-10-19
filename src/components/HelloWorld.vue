ee750b3b83250b86adfca2a8b7da292628c3530c
Ahsan-Ayaz1 <ahsanayaz7755@gmail.com>
2023-10-19 15:07:08 +0500

    Phone Input With Vue3

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { VueTelInput } from "vue3-tel-input";
+import "vue3-tel-input/dist/vue3-tel-input.css";
+
+const phone = ref(null);
+
+// Define an `onInput` function to handle phone input changes
+const onInput = (phone, phoneObject, input) => {
+  // console.log("Phone Object:", phoneObject.formatted);
+  console.log(phone);
+};
+
+onMounted(() => {
+  // Set an initial value for the `phone` ref if needed
+  phone.value = ""; // Replace with your initial phone number
+});
+</script>
+
+<template>
+  <VueTelInput :value="phone" @input="onInput" mode="international"> </VueTelInput>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import VueTelInput from "vue3-tel-input";
+import "vue3-tel-input/dist/vue3-tel-input.css";
+
+const app = createApp(App);
+app.use(VueTelInput);
+app.mount("#app");
